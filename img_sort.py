d14aa2a15858889f812bbeccdb340f42c708412a
veax-void <caxaev95@gmail.com>
2020-02-23 13:41:01 +0800

    distributed everything inside functions, can save img now

--- img_sort.py
+++ img_sort.py
@@ -3,14 +3,17 @@
 """
 Created on Sat Feb 15 18:15:43 2020
 
-@author: veax
+@author: veax-void
 """
-
+# libs
+from matplotlib.image import imread, imsave
+from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import numpy as np
 
+# Functions
 def sort_img(img):
-    print("Compute sort keys")
+    print("Compute sort keys...")
     sorted_img = {} 
     
     for i in range(img.shape[0]):
@@ -19,7 +22,7 @@ def sort_img(img):
             
     sorted_keys = sorted(sorted_img, key=sorted_img.get)
     
-    print("Coping sorted img")
+    print("Coping sorted img...")
     sorted_img = np.zeros(img.shape)
     i = 0
     j = 0
@@ -38,32 +41,49 @@ def sort_img(img):
     
     return sorted_img
 
-img_path = 'img.png'
-img = mpimg.imread(img_path)
+def plot_info(img, sorted_img):
+    print('Plotting extra info...')
+    fig = plt.figure(constrained_layout=True)
 
-# imgplot = plt.imshow(img[:100, :100, 0], clim=(0.8,0.1))
-# plt.colorbar()
+    gs = GridSpec(2,2, fig)
+    
+    ax = []
+    ax += [fig.add_subplot(gs[0,0])]
+    ax += [fig.add_subplot(gs[0,1])]
+    ax += [fig.add_subplot(gs[1,:])]
+    
+    ax[0].imshow(img)
+    ax[0].axis('off')
+    ax[0].set_xlabel('Original')
+    
+    ax[1].imshow(sorted_img)
+    ax[1].axis('off')
+    ax[1].set_xlabel('Sorted')
+    
+    ax[2].hist(img[:,:,0].ravel(), bins=256, range=(0.0, 1.0), alpha=0.4)
+    ax[2].hist(img[:,:,1].ravel(), bins=256, range=(0.0, 1.0), alpha=0.4)
+    ax[2].hist(img[:,:,2].ravel(), bins=256, range=(0.0, 1.0), alpha=0.4)
+    ax[2].set_yscale('log')
+    ax[2].set_xlabel('Tonal variations')
+    ax[2].set_ylabel('Log of frequency')
+    ax[2].set_xlim([-0, 1])
+    
+    fig.suptitle("Image sort")
+    fig.show()
 
-plt.imshow(img)
-# plt.colorbar()
+def main():
+    img_path = 'img.png'
+    
+    img = imread(img_path)
+    
+    sorted_img = sort_img(img)
+    
+    # Some extra info about img
+    plot_info(img, sorted_img) 
+    
+    imsave('output.png', sorted_img)
+    
 
-
-a = sort_img(img)
-
-a_img = a[:,:,0]
-plt.figure()
-plt.imshow(a)
-# plt.colorbar()
-
-plt.figure()
-plt.hist(a_img.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
-
-
-
-
-
-
-
-
+main()
 
 
